877c70498e7713718706f29a5be39fb66b42a79f
frrsfuzn <mochfarrasfauzan@gmail.com>
2023-02-22 09:33:39 +0700

    implement react and install apollo client

--- server/app.js
+++ server/app.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema');
-const mongoose = require('mongoose'); 
-require
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+require;
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://${process.env.MONGOOSEDB_USERNAME}:${process.env.MONGOOSEDB_PASSWORD}@cluster0.tsnz8.mongodb.net/thenetninja?retryWrites=true&w=majority`)
-mongoose.connection.once('open', () => {
-    console.log('connected to database')
-})
+app.use(cors());
 
-app.use('/graphql', graphqlHTTP({
+console.log("username for mongodb:", process.env.MONGODB_USERNAME);
+
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.tsnz8.mongodb.net/thenetninja?retryWrites=true&w=majority`
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: true
-}))
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
-    console.log('server listening to 4000')
-})
+  console.log("server listening to 4000");
+});
